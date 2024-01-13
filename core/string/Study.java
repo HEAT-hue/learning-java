8e7e8f598e42c17f9800857b8e99fc98f595ae96
Emmanuel <onyejemeemmanuel65@gmail.com>
2024-01-13 09:49:51 +0100

    Added a new task

---
+++ core/string/Study.java
@@ -0,0 +1,11 @@
+/**
+ * Study
+ */
+public class Study {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!");
+
+        System.out.println("Arrgghhhh! Here we go again");
+    }
+}
